dd6593aafa7a48e4895291a9f3884ab00fa73fe9
roy <loitp@galaxyjoy.vn>
2024-12-07 21:09:58 +0700

    up

--- app/src/main/java/com/mckimquyen/reader/infrastructure/android/MainActivity.kt
+++ app/src/main/java/com/mckimquyen/reader/infrastructure/android/MainActivity.kt
@@ -48,7 +48,7 @@ class MainActivity : ComponentActivity() {
     override fun onResume() {
         super.onResume()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-//            enableAdaptiveRefreshRate()
+            enableAdaptiveRefreshRate()
         }
     }
 
